bb7a7b83939be1356abc178c2ba46b88fa534ccb
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2024-08-11 19:14:33 -0500

    Feat: Apple-Game-Phaser. Detección de Coliciones con `overlap` - La función `getRandomX`, la pasamos al archivo "utils.js". - En el archivo "create.js" ponemos la colisión de tipo `overlap`
    #14_CollisionDetection

--- phaser-js/create.js
+++ phaser-js/create.js
@@ -1,4 +1,13 @@
+import { getRandomX } from './utils.js';
+
 export function createGame (game) {
+  function targetHit () {
+    game.target.setY(0); // La `apple` reaparece arriba
+    game.target.setX(getRandomX()); // `apple` de forma aleatoria
+    game.points++;
+    console.log(game.points);
+  }
+
   game.add.image(0, 0, 'bg')
     .setOrigin(0, 0); // setOrigin, indica el inicio de la imagen
   const { height } = game.textures.game.config;
@@ -11,6 +20,8 @@ export function createGame (game) {
   game.target = game.physics.add.image(0, 0, 'apple')
     .setOrigin(0, 0);
   game.target.setMaxVelocity(0, game.playerSpeed - 50);
+
+  game.physics.add.overlap(game.target, game.player, targetHit, null, game); // Colisión entre la `apple` y la `basket`
 
   game.cursor = game.input.keyboard.createCursorKeys();
 
---
+++ phaser-js/utils.js
@@ -0,0 +1,3 @@
+export function getRandomX () {
+  return Math.floor(Math.random() * 480);
+}
