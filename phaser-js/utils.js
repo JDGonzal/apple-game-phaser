e3e5681acf274527bfe376860518e69635e1a643
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2024-08-13 10:01:24 -0500

    Feat: Apple-Game-Phaser.Estado de Ganar o Perder - Casi todo recae en la función `gameOver` de "utils.js". - En "main.js" se activa el uso del botón "Inicio". - Mejoras en el archivo "index.html".
    #21_Win_LoseState

--- phaser-js/utils.js
+++ phaser-js/utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 export function getRandomX () {
   return Math.floor(Math.random() * 480);
 }
@@ -5,4 +6,12 @@ export function getRandomX () {
 export function gameOver () {
   console.log('Game Over');
   this.gameOver = true;
+  gameEndScoreSpan.textContent = this.points;
+  if (this.points >= 10) {
+    gameWinLoseSpan.textContent = ' Ganó! 😃';
+  } else {
+    gameWinLoseSpan.textContent = ' Perdió! 😔';
+  }
+  gameEndDiv.style.display = 'flex';
+  this.sys.game.destroy(true);
 }
